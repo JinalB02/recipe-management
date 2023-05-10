2fbc9a1e894b0eb7ac3fddb448138f3364912b2c
jinalb02 <jinalbafna02@gmail.com>
2023-05-10 22:55:42 +0200

    Revert "remove unused code"
    This reverts commit 155e05a0c692a8ea6dc31f12460877ca1f384dcc.

--- src/main/java/com/abnamro/recipe/exceptions/NotFoundException.java
+++ src/main/java/com/abnamro/recipe/exceptions/NotFoundException.java
@@ -6,10 +6,30 @@ public class NotFoundException extends RuntimeException implements ICustomExcept
 
     private HttpStatus status = HttpStatus.NOT_FOUND;
 
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
     public NotFoundException(String message) {
         super(message);
     }
 
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 
     public HttpStatus getStatus() {
         return status;
